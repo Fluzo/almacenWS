369e1e9730bbf4138d3a78f088aa5f9dfdcad932
David Lizurej <davidlizurej@gmail.com>
2022-05-05 21:26:54 +0200

    quitamos wheather

--- AlmacenWS/Controllers/WeatherForecastController.cs
+++ AlmacenWS/Controllers/WeatherForecastController.cs
@@ -18,24 +18,6 @@ namespace AlmacenWS.Controllers
             _logger = logger;
         }
 
-        [HttpGet]
-        [Route("GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
-
-        [HttpGet]
-        [Route("HolaMundo")]
-        public ActionResult<string> HolaMundo()
-        {
-            return Ok("Hola");
-        }
+       
     }
 }
